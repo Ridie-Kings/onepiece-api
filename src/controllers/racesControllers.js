726b06ced821cd080faff2906cbd303a56efceb5
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-08 22:07:40 +0100

    feat: Added races controller & services
    Co-authored-by: juanandub <juanandub@users.noreply.github.com>

---
+++ src/controllers/racesControllers.js
@@ -0,0 +1,28 @@
+import racesServices from "../services/racesServices.js";
+
+export const getAllRaces = (req, res) => {
+    try {
+        const races = racesServices.getAllRaces();
+        res.json(races)
+    } catch (error) {
+        res.status(500).send("Error retrieving races")
+    }
+}
+
+export const getRaceById = (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) {
+            res.status(400).send("ID is not a number");
+            return;
+        }
+        const race = racesServices.getRaceById(id);
+        if (!race) {
+            res.status(404).send("Crew not found")
+            return;
+        }
+        res.json(race)
+    } catch (error) {
+        res.status(400).send("Invalid ID supplied")
+    }
+}
---
+++ src/services/racesServices.js
@@ -0,0 +1,16 @@
+import races from '../../data/races.json' with { type: "json" };
+
+const getAllRaces = () => {
+    const data = races.map(race => race.name);
+    return [...new Set(data)];
+}
+
+const getRaceById = (id) => {
+    const data = races.find(race => race.id === id);
+    return data;
+}
+
+export default {
+    getAllRaces,
+    getRaceById,
+}
