c657784ea35f36271f2bf0cb492fd19f9759dc1e
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-08 22:12:08 +0100

    refactor: Refactor character files to have only one function

--- src/services/charactersServices.js
+++ src/services/charactersServices.js
@@ -1,35 +1,10 @@
 import characters from '../../data/characters.json' with { type: "json" };
-import races from '../../data/races.json' with { type: "json" };
 
 const getAllCharacters = () => characters;
 
 const getCharacterById = (id) => characters.find(character => character.id === id)
 
-const getAllCrews = () => {
-    const crews = characters.map(character => character.crew.name);
-    return [...new Set(crews)];
-}
-
-const getCrewById = (id) => {
-    const data = characters.find(character => character.crew.id === id);
-    return data.crew;
-}
-
-const getAllRaces = () => {
-    const races = characters.map(character => character.race);
-    return [...new Set(races)];
-}
-
-const getRaceById = (id) => {
-    const data = races.find(race => race.name);
-    return data;
-}
-
 export default {
     getAllCharacters,
     getCharacterById,
-    getAllCrews,
-    getCrewById,
-    getAllRaces,
-    getRaceById
 }
