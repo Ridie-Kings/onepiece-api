9b7d7b7ae5c8f7c90d41f094cf81cca6f6594d0c
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-08 13:28:44 +0100

    feat: Added getAllCrews & getCrewById

--- src/controllers/charactersControllers.js
+++ src/controllers/charactersControllers.js
@@ -26,3 +26,30 @@ export const getCharacterById = (req, res) => {
         res.status(400).send("Invalid ID supplied");
     }
 }
+
+export const getAllCrews = (req, res) => {
+    try {
+        const crews = charactersServices.getAllCrews();
+        res.json(crews);
+    } catch (error) {
+        res.status(500).send("Error retrieving crews");
+    }
+}
+
+export const getCrewById = (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) {
+            res.status(400).send("ID is not a number");
+            return;
+        }
+        const crew = charactersServices.getCrewById(id);
+        if (!crew) {
+            res.status(404).send("Crew not found");
+            return;
+        }
+        res.json(crew);
+    } catch (error) {
+        res.status(400).send("Invalid ID supplied");
+    }
+}
--- src/services/charactersServices.js
+++ src/services/charactersServices.js
@@ -4,7 +4,19 @@ const getAllCharacters = () => characters;
 
 const getCharacterById = (id) => characters.find(character => character.id === id)
 
+const getAllCrews = () => {
+    const crews = characters.map(character => character.crew.name);
+    return [...new Set(crews)];
+}
+
+const getCrewById = (id) => {
+    const crew = characters.find(character => character.crew.id === id);
+    return crew.crew.name;
+}
+
 export default {
     getAllCharacters,
-    getCharacterById
+    getCharacterById,
+    getAllCrews,
+    getCrewById
 }
