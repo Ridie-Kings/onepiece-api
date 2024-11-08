6fe0567f102b4d2dd7c221bd78c1ef9a55ce4dce
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-09 00:06:11 +0100

    feat: Added enrichment to character info

--- src/services/charactersServices.js
+++ src/services/charactersServices.js
@@ -1,8 +1,31 @@
 import characters from '../../data/characters.json' with { type: "json" };
+import crews from '../../data/crews.json' with { type: "json" };
+import devilFruits from '../../data/devilFruits.json' with { type: "json" };
+import hakis from '../../data/hakis.json' with { type: "json" };
+import origins from '../../data/origins.json' with { type: "json" };
+import races from '../../data/races.json' with { type: "json" };
+import genders from '../../data/gender.json' with { type: "json" };
 
-const getAllCharacters = () => characters;
+const fillCharacterInfo = (character) => {
+    return {
+        ...character,
+        gender: genders.find(gender => gender.id === character.gender),
+        crew: crews.find(crew => crew.id === character.crew),
+        devil_fruit: devilFruits.find(fruit => fruit.id === character.devil_fruit),
+        haki: character.haki.map(hakiId => hakis.find(haki => haki.id === hakiId)),
+        origin: origins.find(origin => origin.id === character.origin),
+        race: races.find(race => race.id === character.race)
+    };
+};
 
-const getCharacterById = (id) => characters.find(character => character.id === id)
+const getAllCharacters = () => {
+    return characters.map(character => fillCharacterInfo(character));
+};
+
+const getCharacterById = (id) => {
+    const character = characters.find(character => character.id === id);
+    return fillCharacterInfo(character);
+}
 
 export default {
     getAllCharacters,
