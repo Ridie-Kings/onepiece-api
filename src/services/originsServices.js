79a4adebb018a783ff37faed5965c88754f5be58
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-09 00:05:01 +0100

    fix: Fixed origins service misstype

--- src/services/originsServices.js
+++ src/services/originsServices.js
@@ -1,12 +1,12 @@
 import origins from '../../data/origins.json' with { type: "json" };
 
 const getAllOrigins = () => {
-    const data = hakis.map(origins => origins.name);
+    const data = origins.map(origin => origin.name);
     return [...new Set(data)];
 }
 
 const getOriginsById = (id) => {
-    const data = hakis.find(origins => origins.id === id);
+    const data = origins.find(origin => origin.id === id);
     return data;
 }
 
