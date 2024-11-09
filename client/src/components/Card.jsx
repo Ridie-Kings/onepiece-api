11c914d0d7d819d1aadc309c3d44f4933a779c11
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-09 23:50:21 +0100

    style: Refactor Card to imitate an old paper

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,22 +1,31 @@
 import { Anchor } from 'lucide-react';
 export const Card = ({ character }) => {
     return (
-        <article className="border rounded-lg w-64 h-96 p-5 text-center bg-yellow-500">
-            <div className="text-2xl">
-                <h2>WANTED</h2>
-                <h3>Dead or Alive</h3>
+        <article className="relative w-80 p-8 text-center bg-[#fffef0]  shadow-lg paper">
+            <div className="text-3xl text-gray-800 font-serif mb-4">
+                <h2 className='text-6xl'>WANTED</h2>
             </div>
-            <div className="flex items-center justify-center border">
-                <img className="w-40 h-52 -mt-4 rounded-lg transition-transform duration-500 p-[10px] hover:p-0 hover:origin-rigth hover:skew-y-3 hover:scale-110 " src={character.image} alt={character.name} />
+            <div className="flex items-center justify-center mb-4">
+                <img
+                    className="w-40 h-52 rounded-lg transition-transform duration-500 transform  hover:scale-150"
+                    src={character.image}
+                    alt={character.name}
+                />
             </div>
-            <div className="">
-                <h2 className="text-lg my-2">{character.name}</h2>
-                <div className="bg-red-800/90 text-yellow-500 rounded mb-2"><p className="text-lg font-bold my-2">Bounty: {character.bounty}</p></div>
-                <p className='flex justify-center items-center text-base my-1'> <Anchor className='size-4 mr-1' /><strong>Crew:</strong> {character.crew?.name || 'No Crew'}</p>
+            <div>
+                <h3 className='text-3xl tracking-wider'>DEAD OR ALIVE</h3>
+                <h2 className="text-2xl font-bold mb-2 tracking-wider uppercase">{character.name.toUpperCase()}</h2>
+                <div className="bg-red-800/90 text-yellow-500 rounded-lg p-2 mb-4">
+                    <p className="text-lg font-bold">Bounty: {character.bounty}</p>
+                </div>
+                <p className="flex justify-center items-center text-base text-gray-800 gap-1">
+                    <Anchor className="w-5 h-5" />
+                    <strong>Crew:</strong> {character.crew?.name || 'No Crew'}
+                </p>
             </div>
         </article>
     );
-}
+};
 
 import PropTypes from 'prop-types';
 
