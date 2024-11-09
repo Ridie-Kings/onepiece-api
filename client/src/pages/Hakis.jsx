a6b0ceb159746717e28d2cdd04f06ed42ca1eb96
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-09 23:03:06 +0100

    feat: Added routing feature

---
+++ client/src/pages/Hakis.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../components/Navbar"
+
+export const HakisPage = () => {
+    return (
+        <>
+            <Navbar />
+            <div>HakisPage</div>
+        </>
+    )
+}
---
+++ client/src/pages/Origins.jsx
@@ -0,0 +1,10 @@
+import { Navbar } from "../components/Navbar"
+
+export const OriginsPage = () => {
+    return (
+        <>
+            <Navbar />
+            <div>OriginsPage</div>
+        </>
+    )
+}
