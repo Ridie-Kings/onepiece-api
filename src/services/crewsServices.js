9a5502c6f6d65f2dea20752f551adb258ca1513d
juanandub <juanandub@outlook.es>
2024-11-09 11:10:55 +0100

    feat: added getMembersByCrew.service

--- src/services/crewsServices.js
+++ src/services/crewsServices.js
@@ -1,4 +1,5 @@
 import crews from '../../data/crews.json' with { type: "json" };
+import characters from '../../data/characters.json' with { type: "json" };
 
 const getAllCrews = () => {
     const data = crews.map(crew => crew.name);
@@ -10,7 +11,12 @@ const getCrewById = (id) => {
     return data;
 }
 
+const getMembersByCrew = (id) => {
+    const data = characters.find(character => character.crew === id);
+    return [... new Set(data)];
+}
 export default {
     getAllCrews,
     getCrewById,
+    getMembersByCrew
 }
