0435b5ba41aa9307aa153d1aee546a25aaa06241
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-09 19:08:33 +0100

    feat: Added CORS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import apiRoutes from './src/routes/apiRoutes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/', apiRoutes);
 export default app;
