d4a06f2da52810fcfd787ceb537f95eeb41804de
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-09 22:53:23 +0100

    refactor: Changed navbar links & logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Ship, Compass } from 'lucide-react';
+import { Ship, User, Flag, Earth, Grape, Zap, Map, Book } from 'lucide-react';
 import { NavLink } from './NavLink';
 
 export const Navbar = () => {
@@ -6,14 +6,17 @@ export const Navbar = () => {
     return (
         <nav className="bg-gradient-to-r from-red-800 to-red-900 shadow-lg">
             <div className="max-w-7xl mx-auto px-4">
-                <div className="flex items-center justify-between h-16">
-                    <div className="flex items-center space-x-3">
-                        <Ship className="text-yellow-500 w-8 h-8 animate-[bounce_2s_ease-in-out_infinite]" />
-                        <h1 className="text-2xl font-bold text-yellow-500 font-serif">One Piece Characters</h1>
-                    </div>
+                <div className="flex items-center justify-end h-16">
+                    {/* logo de la api */}
                     <ul className="hidden md:flex space-x-8">
                         <NavLink href="/" icon={<Ship className="w-5 h-5" />} text="Home" />
-                        <NavLink href="/characters" icon={<Compass className="w-5 h-5" />} text="Characters" />
+                        <NavLink href="/characters" icon={<User className="w-5 h-5" />} text="Characters" />
+                        <NavLink href="/crews" icon={<Flag className="w-5 h-5" />} text="Crews" />
+                        <NavLink href="/races" icon={<Earth className="w-5 h-5" />} text="Races" />
+                        <NavLink href="/devil-fruits" icon={<Grape className="w-5 h-5" />} text="Devil's Fruits" />
+                        <NavLink href="/hakis" icon={<Zap className="w-5 h-5" />} text="Hakis" />
+                        <NavLink href="/origins" icon={<Map className="w-5 h-5" />} text="Origins" />
+                        <NavLink href="/documentation" icon={<Book className="w-5 h-5" />} text="Docs" />
                     </ul>
                 </div>
             </div>
