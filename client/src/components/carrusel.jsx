92a9374686a3a40b03f2b446eab2ce13a0812d58
juanandub <juanandub@outlook.es>
2024-11-13 11:54:06 +0100

    deleted:    client/src/components/carrusel.css modified:   client/src/components/carrusel.jsx modified:   data/characters.json

--- client/src/components/carrusel.jsx
+++ client/src/components/carrusel.jsx
@@ -1,10 +1,9 @@
-import './carrusel.css';
 export const Carrusel = ({ crew }) => {
   return (
-    <section>
+    <section class="flex w-full h-full">
       {crew.members.map((member, index) => (
         <div key={index}>
-          <img src={member.img}></img>
+          <img src={member.image} class="flex-grow h-96 w-28 object-cover object-top opacity-80 transition ease duration-500 hover:opacity-100 hover:w-80 hover:contrast-120" alt="Character img"></img>
         </div>
       ))}
     </section>
