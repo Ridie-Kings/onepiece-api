f888c2cbb5bb4c1b2738dd45c6371273aa3f0926
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-13 20:57:55 +0100

    refactor: Update crewServices

--- src/services/crewsServices.js
+++ src/services/crewsServices.js
@@ -1,26 +1,59 @@
 import crews from '../../data/crews.json' with { type: "json" };
 import characters from '../../data/characters.json' with { type: "json" };
 
-const fillCrewInfo = (crew) => {
+const fillCrewInfo = () => {
     const data = crews.map(crew => {
-        const members = characters.filter(character => character.crew === crew.id);
-        return { ...crew, members }
+        const members = crew.members.map(member => {
+            const character = characters.find(character => character.id === member.id);
+            return {
+                ...member,
+                ...character
+            };
+        })
+        return {
+            ...crew,
+            members
+        };
     });
     return data;
 }
+
 const getAllCrews = () => {
     return fillCrewInfo();
-
 }
 
 const getCrewById = (id) => {
-    const data = crews.find(crew => crew.id === id);
-    return data;
-}
+    const crew = crews.find(crew => crew.id === id);
+    if (!crew || !crew.members) {
+        return null;
+    }
+    const members = crew.members.map(member => {
+        const character = characters.find(character => character.id === member.id);
+        return {
+            id: member.id,
+            position: member.position,
+            name: character?.name || "Unknown",
+            image: character?.image || null
+        };
+    });
+    return { ...crew, members };
+};
+
 
 const getMembersByCrew = (id) => {
-    const data = characters.filter(character => character.crew === id);
-    return [... new Set(data)];
+    const crew = crews.filter(crew => crew.id === id);
+    const members = crew.members.map(member => {
+        const character = characters.filter(character => character.id === member.id);
+        if (character) {
+            return {
+                id: member.id,
+                position: member.position,
+                ...character
+            };
+        }
+        return member;
+    });
+    return members;
 }
 export default {
     getAllCrews,
