66871280dc2a71dee8934cb59236fbf77cdb1d02
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-13 16:42:23 +0100

    feat: CrewDetails

--- client/src/pages/CrewDetails.jsx
+++ client/src/pages/CrewDetails.jsx
@@ -10,13 +10,15 @@ export const CrewDetails = () => {
     if (!crew) return <div>Crew not found</div>;
 
     return (
-        <main className="w-screen h-screen bg-yellow-950 text-white">
+        <>
             <Navbar />
-            <h1>{crew.name}</h1>
-            <p>{crew.totalBounty} Berris</p>
-            <p>{crew.description}</p>
-            <p>{crew.ship}</p>
-            <Carrusel crew={crew}/>
-        </main>
+            <main className="w-screen h-screen bg-yellow-950 text-white">
+                <h1>{crew.name}</h1>
+                <p>{crew.totalBounty} Berris</p>
+                <p>{crew.description}</p>
+                <p>{crew.ship}</p>
+                <Carrusel crew={crew} />
+            </main>
+        </>
     )
 }
