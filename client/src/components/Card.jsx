e251ce44c9de962e4a45b58a9c857cd2a8844d92
Ridie-Kings <fran6salvatierra@gmail.com>
2024-11-19 21:20:36 +0100

    refactor: Remove article & improve transition w/ albo

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -6,42 +6,41 @@ export const Card = ({ character }) => {
             to={`/characters/${character.id}`}
             relative="path"
             state={{ character }}
+            className="w-80 p-8 text-center bg-[#fffef0] paper"
         >
-            <article className="w-80 p-8 text-center bg-[#fffef0] paper">
-                <div className="text-3xl text-gray-800 font-serif mb-4">
-                    <h2 className="text-6xl">WANTED</h2>
-                </div>
-                <div className="flex items-center justify-center mb-4 group relative aspect-[4/3]">
-                    <img
-                        src={
-                            character.imagebg ||
-                            "https://images.unsplash.com/photo-1534447677768-be436bb09401"
-                        }
-                        alt={character.name}
-                        className="w-full h-full object-cover absolute group-hover:opacity-0 transition-opacity duration-500"
-                    />
-                    <img
-                        src={character.image}
-                        alt={character.name}
-                        className="w-full h-full object-contain absolute opacity-0 group-hover:opacity-100 transition-opacity hover:scale-150  duration-500"
-                    />
-                </div>
-                <div>
-                    <h3 className="text-3xl tracking-wider">DEAD OR ALIVE</h3>
-                    <h2 className="text-2xl font-bold mb-2 tracking-wider uppercase">
-                        {character.name.toUpperCase()}
-                    </h2>
-                    <div className="bg-red-800/90 text-yellow-500 rounded-lg p-2 mb-4">
-                        <div className="text-lg font-bold">
-                            Bounty: {character.bounty}
-                        </div>
+            <div className="text-3xl text-gray-800 font-serif mb-4">
+                <h2 className="text-6xl">WANTED</h2>
+            </div>
+            <div className="flex items-center justify-center mb-4 group relative aspect-[4/3]">
+                <img
+                    src={
+                        character.imagebg ||
+                        "https://images.unsplash.com/photo-1534447677768-be436bb09401"
+                    }
+                    alt={character.name}
+                    className="w-full h-full object-cover absolute group-hover:opacity-0 transition-opacity duration-500"
+                />
+                <img
+                    src={character.image}
+                    alt={character.name}
+                    className="w-full h-full object-contain absolute opacity-0 group-hover:opacity-100 transition-all hover:scale-150  duration-500"
+                />
+            </div>
+            <div>
+                <h3 className="text-3xl tracking-wider">DEAD OR ALIVE</h3>
+                <h2 className="text-2xl font-bold mb-2 tracking-wider uppercase">
+                    {character.name.toUpperCase()}
+                </h2>
+                <div className="bg-red-800/90 text-yellow-500 rounded-lg p-2 mb-4">
+                    <div className="text-lg font-bold">
+                        Bounty: {character.bounty}
                     </div>
-                    <p className="flex justify-center items-center text-base text-gray-800 gap-1">
-                        <Anchor className="w-5 h-5" />
-                        <strong>Crew:</strong> {character.crew?.name || 'No Crew'}
-                    </p>
                 </div>
-            </article>
+                <p className="flex justify-center items-center text-base text-gray-800 gap-1">
+                    <Anchor className="w-5 h-5" />
+                    <strong>Crew:</strong> {character.crew?.name || 'No Crew'}
+                </p>
+            </div>
         </Link>
     );
 };
