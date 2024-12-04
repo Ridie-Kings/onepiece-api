01bf259c4b64e6703abb9d64b71cb1f2233994ad
albocoq <albocoq@gmail.com>
2024-12-04 20:52:57 +0100

    nose

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,43 +1,63 @@
 import { Container } from "../components/Container";
-
+import { motion, useDragControls } from "framer-motion"
 export const HomePage = () => {
     const IMAGES = [
         {
-            src: "https://images.unsplash.com/photo-1587613866194-2f8d3f5e2d7d",
+            src: "https://images.unsplash.com/photo-1534447677768-be436bb09401",
             alt: "A person standing on a mountain during sunset",
             caption: "Sunset on the mountain"
         },
         {
-            src: "https://images.unsplash.com/photo-1587613866194-2f8d3f5e2d7d",
+            src: "https://images.unsplash.com/photo-1534447677768-be436bb09401",
             alt: "A person standing on a mountain during sunset",
             caption: "Sunset on the mountain"
         },
         {
-            src: "https://images.unsplash.com/photo-1587613866194-2f8d3f5e2d7d",
+            src: "https://images.unsplash.com/photo-1534447677768-be436bb09401",
             alt: "A person standing on a mountain during sunset",
             caption: "Sunset on the mountain"
         },
         {
-            src: "https://images.unsplash.com/photo-1587613866194-2f8d3f5e2d7d",
+            src: "https://images.unsplash.com/photo-1534447677768-be436bb09401",
             alt: "A person standing on a mountain during sunset",
             caption: "Sunset on the mountain"
         },
         {
-            src: "https://images.unsplash.com/photo-1587613866194-2f8d3f5e2d7d",
+            src: "https://images.unsplash.com/photo-1534447677768-be436bb09401",
             alt: "A person standing on a mountain during sunset",
             caption: "Sunset on the mountain"
         },
         {
-            src: "https://images.unsplash.com/photo-1587613866194-2f8d3f5e2d7d",
+            src: "https://images.unsplash.com/photo-1534447677768-be436bb09401",
             alt: "A person standing on a mountain during sunset",
             caption: "Sunset on the mountain"
         }
     ]
+
+    const controls = useDragControls()
+    console.log(controls);
+
     return (
         <Container className={""}>
             <h1 className="text-4xl text-yellow-400 flex flex-col items-center">FEATURED SECTIONS</h1>
             <p>Select the section to discover</p>
 
+            <motion.div
+                drag
+                dragControls={controls}
+                className="w-screen overflow-hidden flex gap-10"
+            >
+                {IMAGES.map((image) => (
+                    <motion.img
+                        dragControls={controls}
+
+                        key={image.src}
+                        src={image.src}
+                        width={250}
+                        height={250}
+                    />
+                ))}
+            </motion.div>
         </Container>
     );
 };
