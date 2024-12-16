53a9d9302e69c85d7ffa377f922c7148558f06ab
albocoq <albocoq@gmail.com>
2024-12-16 18:13:25 +0100

    added infinit scroll

--- src/controllers/charactersControllers.js
+++ src/controllers/charactersControllers.js
@@ -1,31 +1,32 @@
 import charactersServices from "../services/charactersServices.js";
 
 export const getAllCharacters = (req, res) => {
-    try {
-        const characters = charactersServices.getAllCharacters();
-        res.set('Cache-Control', 'public, max-age=86400');
-        res.json(characters);
-    } catch (error) {
-        res.status(500).send("Error retrieving characters");
-    }
-}
+  try {
+    const limit = parseInt(req.query.limit);
+    let characters = charactersServices.getAllCharacters();
+    if (!isNaN(limit) && limit > 0) characters = characters.slice(0, limit);
+    res.set("Cache-Control", "public, max-age=86400");
+    res.json(characters);
+  } catch (error) {
+    res.status(500).send("Error retrieving characters");
+  }
+};
 
 export const getCharacterById = (req, res) => {
-    try {
-        const id = parseInt(req.params.id);
-        if (isNaN(id)) {
-            res.status(400).send("ID is not a number");
-            return;
-        }
-        const character = charactersServices.getCharacterById(id);
-        res.set('Cache-Control', 'public, max-age=86400');
-        if (!character) {
-            res.status(404).send("Character not found");
-            return;
-        }
-        res.json(character);
-    } catch (error) {
-        res.status(400).send("Invalid ID supplied");
+  try {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      res.status(400).send("ID is not a number");
+      return;
     }
-}
-
+    const character = charactersServices.getCharacterById(id);
+    res.set("Cache-Control", "public, max-age=86400");
+    if (!character) {
+      res.status(404).send("Character not found");
+      return;
+    }
+    res.json(character);
+  } catch (error) {
+    res.status(400).send("Invalid ID supplied");
+  }
+};
